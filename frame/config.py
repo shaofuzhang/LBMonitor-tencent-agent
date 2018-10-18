02daef41fc7adbcfdea9e911b22943b795a62136
zhangsf <zhangshaof@51idc.com>
2018-10-18 16:05:59 +0800

    add config

---
+++ frame/config.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+__author__ = 'zhangsf'
+
+# 腾讯云API的公共参数
+
+method = 'POST'
+SignatureMethod = 'HmacSHA1'
+Version = '2017-03-12'
+
+# -- open falcon config --
+INTERVAL = 60
+TRANSFER = ["transfer.anchnet.com:6060"]
